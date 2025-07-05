1e2b018260022f74fbc19776bf492123c4397901
ymfsing <ymfsing@outlook.com>
2025-07-05 21:44:23 +0800

    change default ip

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,6 +16,9 @@ git clone https://github.com/theosoft-git/luci-app-easymesh.git package/luci-app
 # Modify default theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+
 # Modify hostname
 sed -i 's/LEDE/iStoreOS/g' package/base-files/files/bin/config_generate
 sed -i 's/LEDE/iStoreOS/g' package/base-files/files/etc/init.d/system
